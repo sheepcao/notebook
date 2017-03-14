9a017b2f5d317c86b884c18de0a03958ec8b49e4
Sheep <caoyang@SheepdeMacBook-Pro.local>
2017-03-14 14:10:34 -0700

    privacy added to info.plist

--- DaysInLine/UIDevice-Hardware.m
+++ DaysInLine/UIDevice-Hardware.m
@@ -272,15 +272,6 @@
 	}
 }
 
-// Illicit Bluetooth check -- cannot be used in App Store
-/* 
-Class  btclass = NSClassFromString(@"GKBluetoothSupport");
-if ([btclass respondsToSelector:@selector(bluetoothStatus)])
-{
-	printf("BTStatus %d\n", ((int)[btclass performSelector:@selector(bluetoothStatus)] & 1) != 0);
-	bluetooth = ((int)[btclass performSelector:@selector(bluetoothStatus)] & 1) != 0;
-	printf("Bluetooth %s enabled\n", bluetooth ? "is" : "isn't");
-}
-*/
+
 
 @end
